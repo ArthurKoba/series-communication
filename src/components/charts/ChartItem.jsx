7f410fc133b07daf5e595c3d5b7dba34f0edd455
Arthur Koba <caprimores@gmail.com>
2023-02-09 00:44:04 +0300

    move register charts to index.js

--- src/components/charts/ChartItem.jsx
+++ src/components/charts/ChartItem.jsx
@@ -2,24 +2,7 @@ import React from 'react';
 
 import {Bar} from 'react-chartjs-2';
 
-import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend,
-} from 'chart.js';
 
-ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend
-);
 
 const ChartItem = ({chart}) => {
     const options = {
--- src/index.js
+++ src/index.js
@@ -4,6 +4,25 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App';
 
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
