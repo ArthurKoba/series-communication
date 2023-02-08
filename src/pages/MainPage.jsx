8eb598d1e1f0f4e4551e07b8a1a77a10f595d0e8
Arthur Koba <caprimores@gmail.com>
2023-02-08 23:17:16 +0300

    update chart data

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -3,15 +3,24 @@ import {Container} from "react-bootstrap";
 import ChartList from "../components/charts/ChartList";
 
 const MainPage = () => {
-
+    const [counter, setCounter] = useState(1)
+    const [data, setData] = useState([counter])
+    const [labels, setLabels] = useState(["123"])
     const [charts, setChart] = useState([
-        {name: "Chart1", labels: ["123"],  data: [1]}
+        {name: "Chart1", labels: labels,  data: data}
     ])
+
+    const updateData = () => {
+        setCounter(counter + 1)
+        setData([counter])
+        setChart([{name: "Chart1", labels: labels,  data: data}])
+    }
 
 
     return (
         <Container fluid>
             <h1 className="text-center">Главная страница</h1>
+            <button onClick={updateData}>test</button>
             <ChartList charts={charts}></ChartList>
         </Container>
     );
