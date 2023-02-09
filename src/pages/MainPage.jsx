a9a92d7a421f4f7d953bdfa4dabb47fc393afbbf
Arthur Koba <caprimores@gmail.com>
2023-02-10 00:12:38 +0300

    delete observer and refactor

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import {Container} from "react-bootstrap";
-import {observer} from "mobx-react-lite";
 
 import ChartList from "../components/charts/ChartList";
 
 
-const MainPage = observer(() => {
+const MainPage = () => {
     return (
-        <Container fluid>
-            <Container fluid className="d-flex row">
-                <h1 className="text-center">Главная страница</h1>
-                <ChartList></ChartList>
-            </Container>
+        <Container fluid className="d-flex row">
+            <h1 className="text-center">Главная страница</h1>
+            {/*<ChartList></ChartList>*/}
         </Container>
     );
-});
+};
 
 export default MainPage;
