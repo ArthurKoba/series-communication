3a3c85f14ad04390dfa044ebb11b84f59ab827e7
Arthur Koba <caprimores@gmail.com>
2023-02-09 18:37:06 +0300

    Add ChartItem.jsx using ref fuctional

--- src/store/chartManagerStorage.js
+++ src/store/chartManagerStorage.js
@@ -1,16 +1,19 @@
 import {makeAutoObservable} from "mobx";
-
+import chartStorage from "./chartStorage";
 
 class ChartManager {
     count = 0
+    charts = []
 
     constructor() {
         makeAutoObservable(this)
+        this.charts.push(
+            new chartStorage()
+        )
     }
 
     increment() {
         this.count += 1
-        console.log("inc")
     }
 }
 
