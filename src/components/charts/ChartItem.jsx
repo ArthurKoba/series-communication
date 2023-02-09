ffa1e02891359aaec635a29975d089b3e1f6a9b0
Arthur Koba <caprimores@gmail.com>
2023-02-09 14:46:52 +0300

    add change types with options and reset scales button

--- src/components/charts/ChartItem.jsx
+++ src/components/charts/ChartItem.jsx
@@ -1,29 +1,45 @@
 import React from "react";
 import {Chart} from 'react-chartjs-2';
-import {Container} from "react-bootstrap";
+import {Button, Container} from "react-bootstrap";
+import Select from "../UI/select/Select";
 
+
+const changeTypeOptions = [
+    {name: "Line", value: "line"}, {name: "Bar", value: "bar"}
+]
 
 class ChartItem extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            type: "bar",
-            name: "123",
-            data: [1,2,3],
-            labels: [],
+            type: props.chart?.type? props.chart.type: "bar",
+            name: props.chart?.name? props.chart.name : "Some chart",
+            data: props.chart?.data? props.chart.data: [],
+            labels: props.chart?.labels? props.chart.labels : [],
+            scales: {y: {min: null, max: null}}
         }
-        this.increment = this.increment.bind(this)
-        this.decrement = this.decrement.bind(this)
+        this.resetScales = this.resetScales.bind(this)
+        this.setData = this.setData.bind(this)
         this.updateData = this.updateData.bind(this)
-        props.setChart(this)
+        this.changeType = this.changeType.bind(this)
     }
 
-    increment() {
-        this.setState({name: "etewt", type: "line"})
+    resetScales() {
+        let min = 0
+        let max = 1
+        if (this.state.data.length) {
+            min = this.state.data.reduce((x, y) => Math.min(x, y))
+            max = this.state.data.reduce((x, y) => Math.max(x, y))
+        }
+        this.setState({scales: {y: {min: min, max: max}}})
     }
 
-    decrement() {
+    setData() {
         this.updateData([1,2,3])
+    }
+
+    changeType(type) {
+        this.setState({type: type})
     }
 
     updateData(data) {
@@ -49,6 +65,7 @@ class ChartItem extends React.Component {
                     text: this.state.name,
                 },
             },
+            scales: this.state.scales,
         }
 
         const data = {
@@ -62,11 +79,12 @@ class ChartItem extends React.Component {
         }
 
         return (
-            <Container fluid className="p-0">
+            <Container fluid className="p-0 col-6">
                 <Chart type={this.state.type} options={options} data={data}></Chart>
-                <Container fluid className="p-0 d-flex justify-content-center">
-                    <button onClick={this.increment}>count+</button>
-                    <button onClick={this.decrement}>count-</button>
+                <Container fluid className="p-0 d-flex  justify-content-center">
+                    <Button onClick={this.resetScales}>Reset scales</Button>
+                    <Button onClick={this.setData}>Set Data</Button>
+                    <Select options={changeTypeOptions} onChange={this.changeType}></Select>
                 </Container>
             </Container>
         )
