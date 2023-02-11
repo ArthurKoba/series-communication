de3d529108506043ddf8946f586949dfa313b5e6
Arthur Koba <caprimores@gmail.com>
2023-02-11 18:59:58 +0300

    add change baudRate, states and variants connect buttons, add state connection disabled button

--- src/components/serial-ports/SerialPortItem.jsx
+++ src/components/serial-ports/SerialPortItem.jsx
@@ -6,8 +6,31 @@ import {observer} from "mobx-react-lite";
 
 const SerialPortItem = observer(({port}) => {
 
-    const changeName = (newName) => newName.length? port.setName(newName) : ""
+    const state = port.isConnected? "Connected" : port.isConnecting? "Connecting" : "Disconnected"
 
+    const getStateVariantButton = () => {
+        let state = "outline-"
+        if (port.isConnected) state += "success"
+        else if (port.isConnecting) state += "warning"
+        else state += "danger"
+        return state
+    }
+
+
+
+    const changeBaudRate = (event) => {
+        let value = Number(event.target.value)
+        switch (event.type) {
+            case "change":
+                port.checkBaudRate(value);
+                break;
+            case "blur":
+                if (port.isBaudRateValid !== true) return
+                port.setBaudRate(value)
+                port.isBaudRateValid = null
+                break;
+        }
+    }
 
     return (
         <Card className="col-4">
@@ -18,31 +41,41 @@ const SerialPortItem = observer(({port}) => {
                 <InputGroup size="sm" className="mb-2">
                     <InputGroup.Text>Name</InputGroup.Text>
                     <Form.Control
-                        onBlur={(e) => changeName(e.target.value)}
+                        onBlur={(e) => port.setName(e.target.value)}
                         placeholder={port.name || "null"}
                         disabled={!port.usbVendorId && !port.usbProductId}
                     />
                 </InputGroup>
                 <InputGroup size="sm" className="mb-2">
                     <InputGroup.Text>BaudRate</InputGroup.Text>
-                    <Form.Control placeholder="115200"/>
+                    <Form.Control isInvalid={port.isBaudRateValid === false}
+                                  onChange={changeBaudRate}
+                                  onBlur={changeBaudRate}
+                                  placeholder="115200"/>
                 </InputGroup>
                 <ButtonGroup className="mb-2">
                     <Button variant={port.isConnected? "outline-secondary": "success"}
-                            size="sm" active={port.isConnected}
+                            size="sm" disabled={port.isBaudRateValid === false || port.isConnected}
+                            active={port.isConnected}
                             onClick={() => port.connect()}>
                         Connect
                     </Button>
                     <Button variant={!port.isConnected? "outline-secondary": "danger"}
-                            size="sm" active={!port.isConnected}
+                            size="sm" disabled={!port.isConnected}
+                            active={!port.isConnected}
                             onClick={() => port.disconnect()}>
                         Disconnect
+                    </Button>
+                </ButtonGroup>
+                <ButtonGroup className="mb-2">
+                    <Button variant={getStateVariantButton()}
+                            disabled={true} size="sm">
+                        {state}
                     </Button>
                 </ButtonGroup>
             </div>
             <div className="card-footer row text-center">
                 <span>usbProductId: {port.usbProductId || "null"}</span>
-                {/*<div className="vr"></div>*/}
                 <hr className="m-2 mx-0"/>
                 <span>usbVendorId: {port.usbVendorId || "null"}</span>
             </div>
--- src/store/serialPortStorage.js
+++ src/store/serialPortStorage.js
@@ -4,7 +4,11 @@ import {makeAutoObservable} from "mobx";
 class SerialPortItemStorage {
 
     isConnected = false
+    isConnecting = false
+    isBaudRateValid = false
+
     name = null
+    baudRate = 0
 
     constructor(portObject) {
         this.portObject = portObject
@@ -15,7 +19,14 @@ class SerialPortItemStorage {
     }
 
     async connect() {
-        this.isConnected = true
+        if (this.isBaudRateValid === false) return
+        this.isConnecting = true
+        setTimeout(() => {
+            this.isConnected = true
+            this.isConnecting = false
+        }, 1000)
+        // this.isConnected = true
+        // console.log(this.isConnected)
     }
 
     async disconnect() {
@@ -23,8 +34,15 @@ class SerialPortItemStorage {
     }
 
     setName(newName) {
-        this.name = newName
+        this.name = newName.length? newName : ""
     }
+
+    setBaudRate(baudRate) {
+        console.log(baudRate)
+        // this.name = newName
+    }
+
+    checkBaudRate = (value) => this.isBaudRateValid = Number.isInteger(value) && value > 1
 }
 
 export default SerialPortItemStorage
