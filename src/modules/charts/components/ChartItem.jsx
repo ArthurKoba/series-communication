145aeb8621e4037508635f0847063bd1286ea3c6
Arthur Koba <caprimores@gmail.com>
2023-02-14 00:03:42 +0300

    use observer, modal remove chart, fullscreen functionality

--- src/modules/charts/components/ChartItem.jsx
+++ src/modules/charts/components/ChartItem.jsx
@@ -1,20 +1,37 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
+import {observer} from "mobx-react-lite";
 import {Button, Container} from "react-bootstrap";
 
-import chartManagerStorage from "../store/chartManagerStorage";
+import ModalRemoveChartButton from "./ui/ModalRemoveChartButton";
 
-const ChartItem = ({chartStorage}) => {
+
+const ChartItem = observer(({chart}) => {
     const ctx = useRef(null)
+    const [showRemoveModal, setShowRemoveModal] = useState(false)
 
-    useEffect(() => chartStorage.setCtx(ctx.current), [])
+    const fullScreen = chart.isFullScreen? "col-12": "col-6"
+
+    useEffect(() => chart.setCtx(ctx.current), [])
     return (
-        <Container fluid className="d-flex row col-8">
-            <canvas ref={ctx}></canvas>
-            <Button onClick={() => chartStorage.click()}>click</Button>
-            <Button onClick={() => chartManagerStorage.deleteChart(chartStorage)}>Удалить</Button>
+        <Container fluid className={"d-flex row border border-2 border-primary rounded p-0 " + fullScreen}>
+            <canvas ref={ctx} className="border-bottom border-primary"></canvas>
+            <Container fluid className="p-2">
+
+                    <Button onClick={() => chart.click()}>click</Button>
+                    <Button onClick={() => (chart.isFullScreen = !chart.isFullScreen)}>FullScreen</Button>
+                    <Button variant="danger" onClick={() => setShowRemoveModal(true)}>
+                        Удалить
+                    </Button>
+
+            </Container>
+            <ModalRemoveChartButton
+                show={showRemoveModal}
+                handleClose={() => setShowRemoveModal(false)}
+                handleConfirm={chart.remove}
+            />
         </Container>
 
     );
-};
+});
 
 export default ChartItem;
