c551031280bc34f2351ad4a2ae61312d0b81a8c3
Arthur Koba <caprimores@gmail.com>
2023-02-15 00:19:18 +0300

    add validation scales

--- src/modules/charts/components/ChartItemConfiguration.jsx
+++ src/modules/charts/components/ChartItemConfiguration.jsx
@@ -1,22 +1,29 @@
 import React, {useState} from 'react';
 import {Button, Container, Form, InputGroup} from "react-bootstrap";
+import {observer} from "mobx-react-lite";
+
 import SelectChartType from "./ui/SelectChartType";
 
-const ChartItemConfiguration = ({chart}) => {
+const ChartItemConfiguration = observer(({chart}) => {
     const [maxScale, setMaxScale] = useState("")
     const [minScale, setMinScale] = useState("")
-    // const [maxScaleInvalid, setMaxScaleInvalid] = useState("")
+    const [minScaleInvalid, setMinScaleInvalid] = useState(false)
+    const [maxScaleInvalid, setMaxScaleInvalid] = useState(false)
 
     const changeMaxScale = (value) => {
         setMaxScale(value)
         value = parseFloat(value)
-        if (value) chart.setMaxScale(value)
+        if (isNaN(value)) return setMaxScaleInvalid(true)
+        setMaxScaleInvalid(false)
+        chart.setMaxScale(value)
     }
 
     const changeMinScale = (value) => {
         setMinScale(value)
         value = parseFloat(value)
-        if (value) chart.setMinScale(value)
+        if (isNaN(value)) return setMinScaleInvalid(true)
+        setMinScaleInvalid(false)
+        chart.setMinScale(value)
     }
 
     return (
@@ -26,17 +33,17 @@ const ChartItemConfiguration = ({chart}) => {
             <InputGroup size="sm" className="mb-2">
                 <InputGroup.Text>Min scale</InputGroup.Text>
                 <Form.Control onChange={(e) => changeMinScale(e.target.value)}
-                              value={minScale}
-                              placeholder="..."/>
+                              value={minScale} isInvalid={minScaleInvalid}
+                              placeholder={chart.data.options.scales.y?.min}/>
             </InputGroup>
             <InputGroup size="sm" className="mb-2">
                 <InputGroup.Text>Max scale</InputGroup.Text>
                 <Form.Control onChange={(e) => changeMaxScale(e.target.value)}
-                              value={maxScale}
-                              placeholder="..."/>
+                              value={maxScale} isInvalid={maxScaleInvalid}
+                              placeholder={chart.data.options.scales.y?.max}/>
             </InputGroup>
         </Container>
     );
-};
+});
 
 export default ChartItemConfiguration;
