b2ea6478be86cd3ed3f743be3ef218512ae9775c
Arthur Koba <caprimores@gmail.com>
2023-02-15 09:02:21 +0300

    add generator functional, setter handler, change length data and delay functional

--- src/modules/generators/store/generatorStorage.js
+++ src/modules/generators/store/generatorStorage.js
@@ -3,13 +3,17 @@ import {makeAutoObservable} from "mobx";
 class Generator {
 
     isEnabled = false
-    frequency = 0
+    handler = null
+    task = null
 
     constructor(configs = {}) {
         this.id = configs.id
         this.manager = configs.manager
         this.frequency = configs?.frequency || 0.1
+        this.lengthData = configs?.lengthData || 10
+        this.delayMs = configs?.delayMs || 10
         this.isEnabled = configs?.isEnabled
+        if (this.isEnabled) this.enable()
         makeAutoObservable(this)
     }
 
@@ -18,9 +22,26 @@ class Generator {
         this.updateConfigs()
     }
 
+    async startTask() {
+        if (this.task !== null) return
+        let index = 0
+        while (this.isEnabled) {
+            if (this.handler !== null) {
+                let data = []
+                let stopIndex = index + this.lengthData
+                for (index; index < stopIndex && this.isEnabled; index++) {
+                    data.push(Math.sin(this.frequency * index))
+                }
+                this.handler({dataType: null, data: data})
+            }
+            await new Promise(resolve => setTimeout(resolve, this.delayMs));
+        }
+    }
+
     updateConfigs() {
         this.manager.updateGeneratorConfig({
-            id: this.id, frequency: this.frequency, isEnabled: this.isEnabled
+            id: this.id, frequency: this.frequency, isEnabled: this.isEnabled,
+            lengthData: this.lengthData, delayMs: this.delayMs
         })
     }
 
@@ -30,6 +51,7 @@ class Generator {
 
     enable() {
         this.isEnabled = true
+        this.task = this.startTask()
         this.updateConfigs()
     }
 
@@ -38,6 +60,17 @@ class Generator {
         this.updateConfigs()
     }
 
+    setLengthData(value) {
+        this.lengthData = value
+        this.updateConfigs()
+    }
+
+    setDelayMs(value) {
+        this.delayMs = value
+        this.updateConfigs()
+    }
+
+    setHandler = (handler) => this.handler = handler
 }
 
 export default Generator;
