3d59121a9adf42a98e97862c3b8ac166b25e1360
Arthur Koba <caprimores@gmail.com>
2023-02-17 22:42:31 +0300

    upgrading cobs, new package character `\n`

--- src/modules/cobs/decoder.js
+++ src/modules/cobs/decoder.js
@@ -1,14 +1,15 @@
-export function cobsDecode (buffer)
-{
+export function cobsDecode (buffer) {
+    const delimiter = 10;
+    const debt = 255-1;
     let dest = [];
-    for (let i = 0; i < buffer.length; ) {
+    let i = delimiter === 0? 1 : 0
+    while (true) {
         let code = buffer[i++];
-        for (let j = 1; j < code; j++) {
-            dest.push(buffer[i++]);
-        }
-        if (code < 0xFF && i < buffer.length) {
-            dest.push(0);
-        }
+        if (code === delimiter) break
+        else if (code === undefined) return []
+        if (delimiter !== 0 && code >= delimiter-1) code++;
+        for (let j = 0; j < code; j++) dest.push(buffer[i++]);
+        if (code < debt && i+1 !== buffer.length) dest.push(delimiter);
     }
     return dest
 }
