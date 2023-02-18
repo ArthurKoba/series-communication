6337d7898d2701e33d42cff0ab714d883e52e321
Arthur Koba <caprimores@gmail.com>
2023-02-18 23:29:59 +0300

    update cobs decoder

--- src/modules/cobs/decoder.js
+++ src/modules/cobs/decoder.js
@@ -1,15 +1,15 @@
 export function cobsDecode (buffer) {
-    const delimiter = 10;
-    const debt = 255-1;
-    let dest = [];
-    let i = delimiter === 0? 1 : 0
-    while (true) {
-        let code = buffer[i++];
-        if (code === delimiter) break
-        else if (code === undefined) return []
-        if (delimiter !== 0 && code >= delimiter-1) code++;
-        for (let j = 0; j < code; j++) dest.push(buffer[i++]);
-        if (code < debt && i+1 !== buffer.length) dest.push(delimiter);
+    const delimiter = 10
+    const debt = 15
+    let dest = []
+    for (let i = 0; i < buffer.length;) {
+        let code = buffer[i++]
+        if (code === undefined) return []
+        if (code === delimiter) return dest
+        if (delimiter !== 0 && code === 0) code = delimiter
+        for (let j = 1; j < code; j++) dest.push(buffer[i++])
+        if (buffer[buffer.length-1] === undefined) return []
+        if (code < debt && i < buffer.length - 1) dest.push(delimiter)
     }
     return dest
 }
