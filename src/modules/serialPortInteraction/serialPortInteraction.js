4a50d454e9b4b5196a70031aa8a8540e7437e29c
Arthur Koba <caprimores@gmail.com>
2023-02-19 23:30:02 +0300

    update serialPortInteraction | universal packet parser

--- src/modules/serialPortInteraction/serialPortInteraction.js
+++ src/modules/serialPortInteraction/serialPortInteraction.js
@@ -1,37 +1,52 @@
+import {PacketDataType} from "./packetDataType";
+
+
 export class SerialPortInteraction {
-    #packetsConfig = {}
+    #packetsNames = {}
     #littleEndianReceive = true
     #byteOffsetReceive = 0
 
-    setPacketConfig(config) {
-        this.#packetsConfig = config
+    setPacketsNames(names) {
+        this.#packetsNames = names
     }
 
     parsePacket(buffer) {
-        console.log(buffer)
         let packet = {}
         packet.number = buffer[0]
-        let sizeView = new DataView(new Uint8Array(buffer.slice(1, 3)).buffer)
-        packet.length = sizeView.getUint16(this.#byteOffsetReceive, this.#littleEndianReceive)
-        let dataView = new DataView(new Uint8Array(buffer.slice(3, buffer.length)).buffer)
-        let packetConfig = this.#packetsConfig[packet.number]
+        packet.name = this.#packetsNames[packet.number] || packet.number.toString()
+        let dataTypeId = buffer[1]
+        packet.dataType = PacketDataType.getDataTypeName(dataTypeId)
+        let sizeView = new DataView(new Uint8Array(buffer.slice(2, 4)).buffer)
+        packet.byteLength = sizeView.getUint16(this.#byteOffsetReceive, this.#littleEndianReceive)
+        let dataView = new DataView(new Uint8Array(buffer.slice(4, buffer.length)).buffer)
         packet.data = []
-        let dataType = packetConfig?.dataType || "uint8_t"
-        for (let offset = 0; offset < packet.length;) {
-            let dataValue = null
-            switch (dataType) {
-                case "float32_t":
-                    dataValue = dataView.getFloat32(offset, this.#littleEndianReceive)
-                    offset += 4
+        for (let offset = 0; offset < packet.byteLength; offset += PacketDataType.getOffset(dataTypeId)) {
+            switch (dataTypeId) {
+                case PacketDataType.INT8_T:
+                    packet.data.push(dataView.getInt8(offset))
                     break
-                case "uint8_t":
+                case PacketDataType.INT16_T:
+                    packet.data.push(dataView.getInt16(offset, this.#littleEndianReceive))
+                    break
+                case PacketDataType.UINT16_T:
+                    packet.data.push(dataView.getUint16(offset, this.#littleEndianReceive))
+                    break
+                case PacketDataType.INT32_T:
+                    packet.data.push(dataView.getInt32(offset, this.#littleEndianReceive))
+                    break
+                case PacketDataType.UINT32_T:
+                    packet.data.push(dataView.getUint32(offset, this.#littleEndianReceive))
+                    break
+                case PacketDataType.FLOAT32_T:
+                    packet.data.push(dataView.getFloat32(offset, this.#littleEndianReceive))
+                    break
+                case PacketDataType.FLOAT64_T:
+                    packet.data.push(dataView.getFloat64(offset, this.#littleEndianReceive))
+                    break
                 default:
-                    dataValue = dataView.getUint8(offset)
-                    offset += 1
+                    packet.data.push(dataView.getUint8(offset))
             }
-            packet.data.push(dataValue)
         }
-        console.log(packet.data)
         return packet
     }
 }
