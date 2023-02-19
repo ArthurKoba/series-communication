a489c1ee8f4212b40f09a8cf7e50ab9ee6c00193
Arthur Koba <caprimores@gmail.com>
2023-02-19 23:30:03 +0300

    add Test packets names

--- src/modules/serialPortInteraction/index.js
+++ src/modules/serialPortInteraction/index.js
@@ -3,9 +3,9 @@ import {PacketDataType} from './packetDataType'
 
 const serialPortInteraction = new SerialPortInteraction()
 
-serialPortInteraction.setPacketConfig({
-    21: {dataName: "FFT FLOAT", dataType: "float32_t"},
-    22: {dataName: "FFT BYTE", dataType: "uint8_t"},
+serialPortInteraction.setPacketsNames({
+    21: "FFT FLOAT",
+    22: "FFT BYTE",
 })
 
 export {
