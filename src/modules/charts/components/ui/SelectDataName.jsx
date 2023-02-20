ed5ad223bd7f4e6de2d2865bd9e1a64107e23234
Arthur Koba <caprimores@gmail.com>
2023-02-21 01:02:35 +0300

    buf fix | disabled default option

--- src/modules/charts/components/ui/SelectDataName.jsx
+++ src/modules/charts/components/ui/SelectDataName.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import {Form} from "react-bootstrap";
+import {observer} from "mobx-react-lite";
 
-const SelectDataName = ({chart}) => {
-    console.log(chart.selectedDataName)
+const SelectDataName = observer(({chart}) => {
+    const needAddUnknownSelect = !!(!chart.availableDataNames.includes(chart.selectedDataName) && chart.selectedDataName)
+
     return (
-        <Form.Select defaultValue={chart.selectedDataName || ""}
-                     onChange={(e) => chart.setDataName(e.target.value)}>
-            <option value="">All data</option>
+        <Form.Select defaultValue={chart.selectedDataName || ""} onChange={(e) => chart.setDataName(e.target.value)}>
+            <option value="">All dataNames</option>
+            {needAddUnknownSelect? <option disabled value={chart.selectedDataName}>{chart.selectedDataName}</option> : ""}
             {
-                chart.availableDataNames.map((name, index) =>
-                    <option key={index} value={name}>{name}</option>)
+                chart.availableDataNames.map((dataName, index) =>
+                    <option
+                        key={index}
+                        value={dataName || ""}>
+                        {dataName || "All dataNames"}
+                    </option>
+                )
             }
         </Form.Select>
     );
-};
+});
 
 export default SelectDataName;
