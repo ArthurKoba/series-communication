a0d6aef5394ec4905f90799b7fd4651ce3728d07
Arthur Koba <caprimores@gmail.com>
2023-02-21 04:45:16 +0300

    add fps indicator color

--- src/modules/charts/components/ChartItem.jsx
+++ src/modules/charts/components/ChartItem.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useRef} from 'react';
 import {observer} from "mobx-react-lite";
-import {Button, Container, InputGroup} from "react-bootstrap";
+import {Button, Container} from "react-bootstrap";
 
 import ChartItemConfiguration from "./ChartItemConfiguration";
 
@@ -11,6 +11,10 @@ const ChartItem = observer(({chart}) => {
     const fullScreen = chart.isFullScreen? "col-12": "col-6"
 
     useEffect(() => chart.setCtx(ctx.current), [])
+
+    let fpsColor = chart.fps > 120? "info" : chart.fps > 90? "primary" : chart.fps > 60? "success" :
+                   chart.fps > 60? "success" : chart.fps > 30? "warning" : "danger"
+
     return (
         <Container className={"chart row p-1 " + fullScreen}>
             <Container className="border border-2 border-primary rounded">
@@ -19,7 +23,7 @@ const ChartItem = observer(({chart}) => {
                         <Button size="sm" onClick={() => chart.resetScales()}>Reset Scales</Button>
                         <Button size="sm" onClick={() => (chart.swapFullscreen())}>FullScreen</Button>
                         <Button size="sm" onClick={() => (chart.swapConfigurationOpened())}>Configuration</Button>
-                        {chart.fps? <InputGroup.Text>FPS: {chart.fps}</InputGroup.Text>: ""}
+                        {chart.fps? <Button size="sm" variant={fpsColor} disabled>FPS: {chart.fps}</Button>: ""}
                 </Container>
                 {chart.isConfigurationOpened? <ChartItemConfiguration chart={chart}/> : ""}
             </Container>
