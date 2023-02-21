b9d56a8885a04a4d8175c757a2053d5711224a4f
Arthur Koba <caprimores@gmail.com>
2023-02-21 19:34:14 +0300

    remove label and borderWidth from default data

--- src/modules/charts/store/chartStorage.js
+++ src/modules/charts/store/chartStorage.js
@@ -6,11 +6,7 @@ import {defaultOptions} from "../configs/chartjsdefault"
 const data = {
     labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
     datasets: [
-        {
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            borderWidth: 1
-        }
+        {data: [12, 19, 3, 5, 2, 3]}
     ]
 }
 
