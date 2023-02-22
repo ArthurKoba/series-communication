f93de05aca34938647b42169a64608bee56ad78a
Arthur Koba <caprimores@gmail.com>
2023-02-22 04:07:30 +0300

    enable Highcharts Boost

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import HighCharts from "highcharts"
+import HighchartsBoost from "highcharts/modules/boost"
+
 import "./app.scss"
 import App from './App';
+
+
+HighchartsBoost(HighCharts)
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
