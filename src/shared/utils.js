50586842b68d7a852153831329c658ecc7e9124a
Arthur Koba <caprimores@gmail.com>
2023-02-23 04:14:03 +0300

    add alert warning virtual serial ports

--- src/shared/utils.js
+++ src/shared/utils.js
@@ -7,3 +7,18 @@ export function isSmallScreen() {
     const {width} = getWindowDimensions()
     return width < 768
 }
+
+export function alertWarningVirtualSerialPort() {
+
+    const alertText = "At the time of the development of this software, " +
+        "the Web Serial API did not allow getting the port name in the OS, " +
+        "and it does not have a USB Product ID and Vendor ID. \n" +
+        "Such ports cannot be distinguished from each other, therefore it is " +
+        "impossible to store different configurations for them (name, speed, etc.).\n" +
+        "Therefore, you can use only one virtual port, when adding a new virtual port, " +
+        "the old ones will be forgotten. To switch a virtual port, add it via the add " +
+        "button, and the current one, if any, will be forgotten. \n\n" +
+        "If the Web Serial API currently allows you to get the port name, contact the developer."
+
+    alert(alertText)
+}
