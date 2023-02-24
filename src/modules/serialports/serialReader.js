1cb8ed25932849c253540c3a77f71b7fa991bec5
Arthur Koba <caprimores@gmail.com>
2023-02-25 02:31:13 +0300

    add `jsdoc` || fix disconnect error, add private property and methods

--- src/modules/serialports/serialReader.js
+++ src/modules/serialports/serialReader.js
@@ -2,15 +2,34 @@ import {cobsDecoder} from "../cobs/decoder";
 import {serialPortInteraction} from "../serialPortInteraction";
 
 
+/**
+ * public
+ * @property {SerialPort} port
+ */
 export class SerialReader {
-    constructor({port}) {
+
+    /**
+     * @private
+     * @type {function({dataName: string, data: number[]}) || undefined}
+     */
+    #_handler = undefined
+
+    constructor(port) {
         this.port = port
-        this.isPortOpened = false
-        this.handler = undefined
     }
 
-    setHandler = (handler) => this.handler = handler
+    /**
+     * @public
+     * @param {function({dataName: string, data: number[]}) || undefined} handler
+     */
+    setHandler = (handler) => this.#_handler = handler
 
+
+    /**
+     * @public
+     * @param {number} baudRate
+     * @returns {Promise<void>}
+     */
     async openPort(baudRate) {
         await this.port.open({
             baudRate: baudRate,
@@ -20,75 +39,109 @@ export class SerialReader {
             bufferSize: 4096*4,
             flowControl: "none",
         })
-        this.isPortOpened = true
     }
 
-    async stopRequest() {
-        if (!this.isPortOpened || this.isBusyClosing) return
-        this.isBusyClosing = true
-        if (this.readerTask) {
-            this.needStop = true
-            if (this.reader) this.reader = await this.reader.cancel()
-            await this.readerTask
+    /**
+     * @public
+     * @returns {Promise<void>}
+     */
+    async startReader() {
+        try {
+            /**
+             * @type {Promise<void> || undefined}
+             * @private
+             */
+            this._readerTask = this._read()
+            await this._readerTask
+        } catch (e) {
+            console.error(e)
         }
-        if (this.isPortOpened) {
-            while (this.port.readable?.locked) {
+        await this.stopRequest()
+    }
+
+    /**
+     * @public
+     * @returns {Promise<void>}
+     */
+    async stopRequest() {
+        if (!this.port.readable || this._isBusyClosing) return
+        /**
+         * @type {boolean}
+         * @private
+         */
+        this._isBusyClosing = true
+        if (this._readerTask) {
+            this._needStop = true
+            if (this.port.readable.locked) this.reader = await this.reader.cancel()
+            await this._readerTask
+        }
+        if (this.port.readable) {
+            while (this.port.readable.locked) {
                 await new Promise((resolve) => setTimeout(resolve, 10))
             }
             this.port.close()
-            this.isPortOpened = false
         }
-        this.isBusyClosing = false
+        this._isBusyClosing = false
     }
 
-    async startReader() {
-        try {
-            this.readerTask = this.read()
-            await this.readerTask
-        } catch (e) {
-            console.error(e)
-            await this.stopRequest()
-        }
-    }
 
-    dataHandler(buffer) {
+    /**
+     * @param {number[]} buffer
+     * @private
+     */
+    _dataHandler(buffer) {
+
+        /**
+         * @type {number[]}
+         */
         let cobsData = cobsDecoder.decode(buffer);
-        if (cobsData.length && this.handler) {
+        if (cobsData.length && this.#_handler) {
             let packet
             try {
                 packet = serialPortInteraction.parsePacket(cobsData)
             } catch (e) {
                 console.warn("packet parse failed")
             }
-            if (packet) this.handler({dataName: packet.name, data: packet.data})
+            if (packet) this.#_handler({dataName: packet.name, data: packet.data})
         } else {
+            // todo fix types
             const byteBuffer = new Uint8Array(buffer, 0, buffer.length)
             const string = new TextDecoder().decode(byteBuffer)
             console.log(string)
         }
     }
 
-    async read() {
+    /**
+     * Data reading function. It is launched only with an open port and the `negative parameter of this.needStop`.
+     * Stop is carried out using the `needStop` variable.
+     * Interruption of reading (more precisely endless data expectation) occurs by canceling the this.reader.
+     * @returns {Promise<void>}
+     * @private
+     */
+    async _read() {
         let buffer = []
-        this.needStop = null
-        while (this.needStop !== true && this.port.readable) {
-            if (!this.reader) this.reader = this.port.readable.getReader()
+        this._needStop = null
+        while (this._needStop !== true && this.port.readable) {
+            if (!this.port.readable.locked) this.reader = this.port.readable.getReader()
             let result
             try {
+                // todo Transform Stream
                 result = await this.reader.read()
             } catch (e) {
+                if (e.message.includes("The device has been lost")) {
+                    return this.port.close()
+                }
                 console.warn("Serial Reader: ", e.message)
-                this.reader = undefined
             }
             if (!result || !result.value) continue
             for (let byte of result.value) {
                 buffer.push(byte)
                 if (byte === cobsDecoder.getDelimiter() || buffer.length > 65536 + 5) {
-                    this.dataHandler(buffer)
+                    this._dataHandler(buffer)
                     buffer = []
                 }
             }
         }
-        if (this.reader) this.reader = await this.reader.cancel()
+        if (this.port.readable?.locked) this.reader = await this.reader.cancel()
     }
 }
